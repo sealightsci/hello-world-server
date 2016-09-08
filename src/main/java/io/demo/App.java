b0aef868e6165dd6068e37d2a5a4d35d27a5d93a
sealightsci <sealightsci@gmail.com>
2016-09-08 18:29:16 +0300

    Update App.java

--- src/main/java/io/demo/App.java
+++ src/main/java/io/demo/App.java
@@ -33,7 +33,7 @@ public class App {
         try {
             jettyServer.start();
             //jettyServer.join();
-            Thread.sleep(1000*60);
+            Thread.sleep(1000*60*3);
             jettyServer.stop();
         }
         catch(Exception e)
